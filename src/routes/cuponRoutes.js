fab19bd7a4cfe6e2a1a81724b9dce6af415df0bd
Jose Daniel Gastelu Sotelo <danielgastelu.s@gmail.com>
2025-12-16 16:31:00 -0500

    refeat: new method verify user back

--- src/routes/cuponRoutes.js
+++ src/routes/cuponRoutes.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
-// Verifica que las mayúsculas sean exactas a la imagen image_f85dc1.png
-const getVerifyUser = require('../GET/getVerifyUser'); 
-
-router.get('/verify-user/:id', getVerifyUser);
-
+const getCouponValidity = require('../GET/getCouponValidity');
+router.get('/vigencia/:userId', getCouponValidity);
 module.exports = router;
